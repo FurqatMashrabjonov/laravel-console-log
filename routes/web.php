32e64d1110f845d5c623a563873554c6d2e63edc
FurqatMashrabjonov <FurqatMashrabjonov@users.noreply.github.com>
2024-09-02 12:00:37 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Furqat\LaravelConsoleLog\Http\Controllers\LaravelConsoleLogController;
-use Furqat\LaravelConsoleLog\LaravelConsoleLogServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 Route::get(config('console-log.route-prefix'), LaravelConsoleLogController::class)->name('laravel-console-log');
